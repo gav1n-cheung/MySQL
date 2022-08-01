48c486f94ed66081a3a4a8313909181441931eaa
gavin cheung <gavinbr@outlook.com>
2022-08-01 15:56:25 +0800

    Update main.cpp

--- SQLTest/main.cpp
+++ SQLTest/main.cpp
@@ -10,88 +10,44 @@ using namespace std;
 
 int main() {
     cout<<setiosflags(ios::fixed)<<setprecision(2);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::io::loadPCDFile("/home/scy/script/gittest/MySQL/SQLTest/scans.pcd", *cloud);
-//    pcl::visualization::CloudViewer viewer("input file visualization");
-//    viewer.showCloud(cloud);
-//    while(!viewer.wasStopped()){}
-    double t1 = omp_get_wtime();
-    MYSQL mysql;
-    mysql_init(&mysql);
-    const string host = "xx.xx.xx.xx";
-    const string user = "sqlusername";
-    const string password = "sqluserpassword";
-    const string db = "sqldbname";
-    if (!mysql_real_connect(&mysql, host.c_str(), user.c_str(), password.c_str(), db.c_str(), 3306, 0, 0))
-        cout << "connect mysql failed!" << mysql_error(&mysql) << endl;
-    else
-        cout << "connect mysql success!" << endl;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::io::loadPCDFile("0scans.pcd", *cloud);
+    cout<<cloud->points[0].x<<" "<<cloud->points[0].y<<" "<<cloud->points[0].z<<endl;
+     double t1 = omp_get_wtime();
+     MYSQL mysql;
+     mysql_init(&mysql);
+     const string host = "localhost";
+     const string user = "cheung";
+     const string password = "128veg8A@";
+     const string db = "coal_data";
+     if (!mysql_real_connect(&mysql, host.c_str(), user.c_str(), password.c_str(), db.c_str(), 3306, 0, 0))
+         cout << "connect mysql failed!" << mysql_error(&mysql) << endl;
+     else
+         cout << "connect mysql success!" << endl;
     double t2 = omp_get_wtime();
     int index = 0;
-    string sql = "insert into table3(x,y,z) values";
+    string sql = "insert into point_cloud_data(x,y,z,r,g,b) values";
     for (auto item: *cloud) {
         char tmp[8*100];
-        sprintf(tmp,"(%f,%f,%f),",item.x,item.y,item.z);
+        sprintf(tmp,"(%.3f,%.3f,%.3f,%d,%d,%d),",item.x,item.y,item.z,(int)item.r,(int)item.g,(int)item.b);
         sql+=tmp;
         index++;
-//        cout<<sql<<endl;
-//        string sql = "insert into table3(x,y,z) values("+item.x+","+item.y+","+item.z+")";
-//        else cout << "mysql_real_query success! " << endl;
-//        break;
-        if(index>100000) {
+
+        if(index>50000) {
             cout<<index<<endl;
             sql.pop_back();
-            if (mysql_query(&mysql, sql.c_str())) cout << "mysql_real_query faild!" << endl;
+            if (mysql_query(&mysql, sql.c_str())) perror("sql query");
             else cout<<"query success"<<endl;
-            sql = "insert into table3(x,y,z) values";
+            sql = "insert into point_cloud_data(x,y,z,r,g,b) values";
             index=0;
         }
     }
     sql.pop_back();
-    if (mysql_query(&mysql, sql.c_str())) cout << "mysql_real_query faild!" << endl;
+    if (mysql_query(&mysql, sql.c_str())) perror("sql query");
     else cout<<"query success"<<endl;
-    sql = "insert into table3(x,y,z) values";
-    double t3 = omp_get_wtime();
+    sql = "insert into point_cloud_data(x,y,z,r,g,b) values";
 
-//    cout<<sql<<endl;
-
-//    cout << x.size() << endl;
-//    cout<<x<<endl;
-//    string sql = "insert into table2(x) values('"+x+"')";
-//    int res = mysql_real_query(&mysql,sql.c_str(),sizeof(sql));
-//    res =mysql_query(&mysql,sql.c_str());
-//    if(!res) cout<<"mysql_real_query faild!"<<sql<<endl;
-//    else cout<<"mysql_real_query success! "<<sql<<endl;
-//
-//    string sql1 = "select * from table2";
-//    int res1 = mysql_real_query(&mysql,sql1.c_str(),sizeof(sql1));
-//    res1 =mysql_query(&mysql,sql.c_str());
-//    if(!res1) cout<<"mysql_real_query faild!"<<sql1<<" "<<mysql_error(&mysql)<<endl;
-//    else cout<<"mysql_real_query success! "<<sql1<<endl;
-//
-//
-//    MYSQL_RES* result = mysql_use_result(&mysql);
-//    // MYSQL_RES* result = mysql_store_result(&mysql);
-//    if(!result) cout<<"mysql_use_result failed! "<<mysql_error(&mysql)<<endl;
-//    MYSQL_FIELD *field = 0;
-//    while(field=mysql_fetch_field(result)) cout<<"key: "<<field->name<<endl;
-//    int fnum = mysql_num_fields(result);
-//
-//    MYSQL_ROW row;
-//    while(row=mysql_fetch_row(result)){
-//        unsigned long * lens = mysql_fetch_lengths(result);
-//        for(int i=0;i<fnum;i++){
-//            cout<<mysql_fetch_field_direct(result,i)->name<<":";
-//            if(row[i]) cout<<row[i];
-//            else cout<<"NULL";
-//            cout<<",";
-//        }
-//        cout<<"\n==============================="<<endl;
-//    }
     mysql_close(&mysql);
-
-    std::cout<<t2-t1<<std::endl;
-    std::cout<<t3-t2<<std::endl;
 
     return 0;
 }
